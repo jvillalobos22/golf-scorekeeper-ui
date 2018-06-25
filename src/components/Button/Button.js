f861e410dfeae84526b388899b3e57f53fc940b4
Juan Villalobos <juan@ibm.com>
2018-06-25 15:28:19 -0700

    Created GhostButton

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,19 +4,27 @@ import './Button.css';
 const Button = ({ children, onClick, className }) => {
   const getOnClick = () => {
     if (onClick) {
-      return onClick
+      return onClick;
     } else {
-      return () => {}
+      return () => {};
     }
-  }
+  };
 
   const getButtonClass = () => {
     return className ? `button ${className}` : 'button';
-  }
-  
+  };
+
   return (
-    <button onClick={getOnClick()} type="button" className={getButtonClass()}>{children}</button> 
-  )
-}
+    <button onClick={getOnClick()} type="button" className={getButtonClass()}>
+      {children}
+    </button>
+  );
+};
+
+const GhostButton = ({ className, ...rest }) => {
+  const derivedClass = className ? `ghost_button ${className}` : 'ghost_button';
+  return <Button className={derivedClass} {...rest} />;
+};
 
 export default Button;
+export { GhostButton };
